0e16d76ac45f67cbfff90a0267f1a38c610b1bdb
Divyanshi <guptadivyanshi15@gmail.com>
2026-04-28 23:48:55 +0530

    m

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not set. Add your MongoDB connection string in Render.");
+    }
+
     await connectDB();
 
     app.listen(PORT, () => {
